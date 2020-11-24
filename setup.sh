4595abba49cf029bdaf544e5ef67d39de3f253b1
Ken Chen <zlchen.ken@gmail.com>
2020-11-23 23:27:40 -0800

    add more setup procedures

--- setup.sh
+++ setup.sh
@@ -14,3 +14,15 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # /Users/kc/.vim/bundle/ListToggle/plugin/listtoggle.vim
 # change <leader>q -> <leader>h
+
+# update ~/.zprezto/runcoms/zpreztorc
+# zstyle ':prezto:load' pmodule
+# ...
+#   'completion' \
+#   'git' \
+#   'syntax-highlighting' \
+#   'history-substring-search' \
+#   'prompt'
+
+# zstyle ':prezto:module:editor' key-bindings 'vi'
+# zstyle ':prezto:module:git:status:ignore' submodules 'all'
